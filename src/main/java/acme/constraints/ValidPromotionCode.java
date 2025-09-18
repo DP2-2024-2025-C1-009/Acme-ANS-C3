c750816ea8b9230a4cd1825d68aa0eceeded5aee
Carlos Gallero <gallerolajara@gmail.com>
2025-09-18 14:07:54 +0200

    Pequeñas correciones en validadores de airline y aircraft

--- src/main/java/acme/constraints/ValidPromotionCode.java
+++ src/main/java/acme/constraints/ValidPromotionCode.java
@@ -1,7 +1,6 @@
 
 package acme.constraints;
 
-import java.lang.annotation.Documented;
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -9,15 +8,12 @@ import java.lang.annotation.Target;
 
 import javax.validation.Constraint;
 import javax.validation.Payload;
-import javax.validation.ReportAsSingleViolation;
 
 @Constraint(validatedBy = PromotionCodeValidator.class)
 @Target({
 	ElementType.FIELD
 })
-@Documented
 @Retention(RetentionPolicy.RUNTIME)
-@ReportAsSingleViolation
 public @interface ValidPromotionCode {
 
 	String message() default "";
